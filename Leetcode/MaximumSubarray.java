075ed8c388b5d6541bbf664fb9df2b4eb403b1b2
sakshi-011 <sakshi.singhania011@gmail.com>
2022-07-14 23:04:22 +0530

    Leetcode Solutions

---
+++ Leetcode/MaximumSubarray.java
@@ -0,0 +1,26 @@
+
+package Leetcode;
+
+import java.util.Scanner;
+
+public class MaximumSubarray {
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(),max=0;
+        int[] nums = new int[n];
+        
+        for(int i=0;i<n;i++)
+            nums[i] = in.nextInt();
+        
+        int max_so_far = nums[0];
+        int curr_max = nums[0];
+
+        for (int i = 1; i < nums.length; i++)
+        {
+             curr_max = Math.max(nums[i], curr_max+nums[i]);
+             max_so_far = Math.max(max_so_far, curr_max);
+        }
+        System.out.println(max_so_far);
+    }
+}
---
+++ Leetcode/TwoSum.java
@@ -0,0 +1,34 @@
+package Leetcode;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class TwoSum{
+    
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        HashMap<Integer,Integer> h = new HashMap<Integer,Integer>();
+        for(int i =0;i<nums.length;i++){
+            if(h.containsKey(target-nums[i]))
+            {
+                ans[1] = i;
+                ans[0] = h.get(target-nums[i]);
+                return ans;
+            }
+            h.put(nums[i],i);
+        }
+        return ans;
+    }
+    
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] nums = new int[n];
+        for(int i=0;i<n;i++)
+            nums[i] = in.nextInt();
+        int target = in.nextInt();
+        int[] ans = twoSum(nums,target);
+        System.out.println(ans[0]+","+ans[1]);
+    }
+}
