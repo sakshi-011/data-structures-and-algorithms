6e34bc1b3fbaf7e42484f8a55eb792b476a534bc
sakshi-011 <sakshi.singhania011@gmail.com>
2022-09-23 21:52:06 +0530

    Leetcode Solutions

---
+++ Leetcode/OneToHundred/ReverseInteger.java
@@ -0,0 +1,25 @@
+package Leetcode.OneToHundred;
+
+import java.util.Scanner;
+
+public class ReverseInteger {
+    public static int reverse(int x) {
+        int rev = x%10, max=Integer.MAX_VALUE, min=Integer.MIN_VALUE;
+        x = x/10;
+        while(x!=0){
+            if((rev > max/10) || ((rev == max/10) && (x%10 > 7)))
+                return 0;
+            else if((rev < min/10) || ((rev == min/10) && (x%10 < -8)))
+                return 0;
+            else
+                rev = rev*10+ x%10;
+            x = x/10;
+        }
+        return rev;
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(reverse(x));
+    }
+}
