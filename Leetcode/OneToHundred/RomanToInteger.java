64487eb333ad16c04d40f2cf2275f6063038b2c4
sakshi-011 <sakshi.singhania011@gmail.com>
2022-09-24 20:54:12 +0530

    Leetcode Solutions

---
+++ Leetcode/OneToHundred/RomanToInteger.java
@@ -0,0 +1,39 @@
+package Leetcode.OneToHundred;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class RomanToInteger {
+
+    public static boolean subtract(char x, char y){
+        if((x=='I' && (y=='V' || y=='X')) || (x=='X' && (y=='L' || y=='C')) || (x=='C' && (y=='D' || y=='M')))
+            return true;
+        return false;
+    }
+
+    public static int romanToInt(String s) {
+        if(s.length() < 1 || s.length() > 15)
+            return 0;
+        int i,sum=0;
+        HashMap<Character,Integer> roman = new HashMap<>();
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+
+        for(i = 0; i < s.length(); i++){
+            sum += i<s.length()-1 && subtract(s.charAt(i),s.charAt(i+1)) ? -roman.get(s.charAt(i)) : roman.get(s.charAt(i));
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int x = romanToInt(s);
+        System.out.println(x);
+    }
+}
