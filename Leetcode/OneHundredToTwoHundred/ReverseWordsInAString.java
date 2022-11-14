19163c6326bf89dc1f6b95c06d319601182583cd
sakshi-011 <sakshi.singhania011@gmail.com>
2022-11-14 16:16:11 +0530

    Leetcode Solutions

---
+++ Leetcode/OneHundredToTwoHundred/ReverseWordsInAString.java
@@ -0,0 +1,22 @@
+package Leetcode.OneHundredToTwoHundred;
+
+import java.util.Scanner;
+
+public class ReverseWordsInAString {
+
+    public static String reverseWords(String s){
+        String res = "";
+        String words[] = s.trim().replaceAll("[ ]+", " ").split(" ");
+        for(String x : words){
+            res = x+" "+res;
+        }
+        return res.trim();
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.println(reverseWords(s));
+    }
+
+}
