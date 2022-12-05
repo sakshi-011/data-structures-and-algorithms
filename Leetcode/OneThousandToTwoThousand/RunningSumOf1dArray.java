4918265195a6e3961a3676a91c75e1c8e4d16886
sakshi-011 <sakshi.singhania011@gmail.com>
2022-12-06 03:23:40 +0530

    Leetcode Solutions

---
+++ Leetcode/OneThousandToTwoThousand/RunningSumOf1dArray.java
@@ -0,0 +1,30 @@
+package Leetcode.OneThousandToTwoThousand;
+
+import java.util.Scanner;
+
+public class RunningSumOf1dArray {
+
+    public static int[] runningSum(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i-1]+nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String[] str = s.replaceAll("\\[", "").replaceAll("]", "").split(",");
+        int[] nums = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            nums[i] = Integer.valueOf(str[i]);
+        }
+        int[] ans = runningSum(nums);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i]+",");
+        }
+    }
+
+}
