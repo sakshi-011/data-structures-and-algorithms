4c9591e5ce83c519b01378986f4e27723857c726
sakshi-011 <sakshi.singhania011@gmail.com>
2023-01-29 22:13:21 +0530

    Leetcode Solutions

---
+++ Leetcode/OneThousandToTwoThousand/DivisorGame.java
@@ -0,0 +1,17 @@
+package Leetcode.OneThousandToTwoThousand;
+
+import java.util.Scanner;
+
+public class DivisorGame {
+
+    public static boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(divisorGame(n));
+    }
+
+}
