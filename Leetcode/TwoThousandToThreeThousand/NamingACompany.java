fe159de6544a4a33bacf9e909f4b9cd4b72653e4
sakshi-011 <sakshi.singhania011@gmail.com>
2023-02-09 18:28:00 +0530

    Leetcode Solutions

---
+++ Leetcode/TwoThousandToThreeThousand/NamingACompany.java
@@ -0,0 +1,39 @@
+package Leetcode.TwoThousandToThreeThousand;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class NamingACompany {
+
+    public static long distinctNames(String[] ideas) {
+        int n = ideas.length;
+        Set<String>[] dict = new HashSet[26];
+        for (int i = 0; i < 26; i++) {
+            dict[i] = new HashSet<>();
+        }
+        for (String s : ideas) {
+            dict[s.charAt(0) - 'a'].add(s.substring(1));
+        }
+
+        long ans = 0;
+        for (int i = 0; i < 25; i++) {
+            for (int j = i+1; j < 26; j++) {
+                long common = 0;
+                for(String s : dict[i])
+                    if(dict[j].contains(s))
+                        common++;
+                ans += 2 * (dict[i].size() - common) * (dict[j].size() - common);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String[] ideas = s.replaceAll("\\[\"","").replaceAll("\"]","").split("\",\"");
+        System.out.println(distinctNames(ideas));
+    }
+
+}
