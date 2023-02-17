8f70800a14647452a71c0b2d9e584e5a4f5d6483
sakshi-011 <sakshi.singhania011@gmail.com>
2023-02-17 22:07:06 +0530

    Leetcode Solutions

---
+++ Leetcode/OneToThousand/FiveHundredToSixHundred/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,33 @@
+package Leetcode.OneToThousand.FiveHundredToSixHundred;
+
+import Leetcode.TreeNode;
+
+public class MinimumAbsoluteDifferenceInBST {
+
+    static int min = Integer.MAX_VALUE;
+    static TreeNode prev;
+
+    public static void inorder(TreeNode root){
+        if(root == null)
+            return;
+        inorder(root.left);
+        if(prev != null)
+            min = Math.min(min, root.val - prev.val);
+        prev = root;
+        inorder(root.right);
+
+    }
+
+    public static int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return min;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        root.right = new TreeNode(6);
+        System.out.println(getMinimumDifference(root));
+    }
+
+}
