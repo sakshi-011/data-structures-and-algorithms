39a536a02f5f23ab7c12ab6b735d3ba38fd67c88
sakshi-011 <sakshi.singhania011@gmail.com>
2023-03-06 22:45:45 +0530

    Leetcode Solutions

---
+++ Leetcode/OneThousandToTwoThousand/KthMissingPositiveNumber.java
@@ -0,0 +1,29 @@
+package Leetcode.OneThousandToTwoThousand;
+
+import java.util.Scanner;
+
+public class KthMissingPositiveNumber {
+
+    public static int findKthPositive(int[] arr, int k) {
+        for(int i : arr){
+            if(i <= k)
+                k++;
+            else
+                break;
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String[] str = s.replaceAll("\\[", "").replaceAll("]", "").split(",");
+        int[] arr = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            arr[i] = Integer.valueOf(str[i]);
+        }
+        int k = in.nextInt();
+        System.out.println(findKthPositive(arr,k));
+    }
+
+}
