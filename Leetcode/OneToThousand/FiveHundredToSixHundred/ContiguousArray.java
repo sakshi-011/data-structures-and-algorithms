617715e8d3b8fe56b5e3644789d41c6d039073b6
sakshi-011 <sakshi.singhania011@gmail.com>
2024-03-16 23:29:36 +0530

    Leetcode Solutions

---
+++ Leetcode/OneToThousand/FiveHundredToSixHundred/ContiguousArray.java
@@ -0,0 +1,36 @@
+package Leetcode.OneToThousand.FiveHundredToSixHundred;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ContiguousArray {
+
+    public static int findMaxLength(int[] nums) {
+        int[] arr = new int[2 * nums.length + 1];
+        Arrays.fill(arr, -2);
+        arr[nums.length] = -1;
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 0 ? -1 : 1);
+            if (arr[count + nums.length] >= -1) {
+                maxlen = Math.max(maxlen, i - arr[count + nums.length]);
+            } else {
+                arr[count + nums.length] = i;
+            }
+
+        }
+        return maxlen;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String[] st = s.replaceAll("\\[","").replaceAll("]","").split(",");
+        int[] nums = new int[st.length];
+        int i = 0;
+        for(String t : st){
+            nums[i++] = Integer.parseInt(t);
+        }
+        System.out.println(findMaxLength(nums));
+    }
+}
