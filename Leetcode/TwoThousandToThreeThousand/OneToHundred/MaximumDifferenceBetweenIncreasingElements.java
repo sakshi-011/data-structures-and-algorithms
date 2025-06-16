8d83aa3d4aca03808b531feef9e73154455deee2
sakshi-011 <sakshi.singhania011@gmail.com>
2025-06-16 23:18:07 +0530

    Leetcode Solutions

---
+++ Leetcode/TwoThousandToThreeThousand/OneToHundred/MaximumDifferenceBetweenIncreasingElements.java
@@ -0,0 +1,30 @@
+package Leetcode.TwoThousandToThreeThousand.OneToHundred;
+
+import java.util.Scanner;
+
+public class MaximumDifferenceBetweenIncreasingElements {
+
+    public static int maximumDifference(int[] nums) {
+        int n = nums.length;
+        int ans = -1, premin = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > premin) {
+                ans = Math.max(ans, nums[i] - premin);
+            } else {
+                premin = nums[i];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String[] st = s.replaceAll("\\[","").replaceAll("]","").split(",");
+        int[] nums = new int[st.length];
+        for(int i = 0; i < st.length; i++){
+            nums[i] = Integer.parseInt(st[i]);
+        }
+        System.out.println(maximumDifference(nums));
+    }
+}
