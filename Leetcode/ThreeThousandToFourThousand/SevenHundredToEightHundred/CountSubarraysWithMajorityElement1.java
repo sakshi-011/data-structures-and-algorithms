6305111fcc039ce673ac5c47183361166a51c14c
sakshi-011 <sakshi.singhania011@gmail.com>
2026-06-25 11:07:37 +0530

    Leetcode Solutions

---
+++ Leetcode/ThreeThousandToFourThousand/SevenHundredToEightHundred/CountSubarraysWithMajorityElement1.java
@@ -0,0 +1,33 @@
+package Leetcode.ThreeThousandToFourThousand.SevenHundredToEightHundred;
+
+import java.util.Scanner;
+
+public class CountSubarraysWithMajorityElement1 {
+
+    public static int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = i; j < n; ++j) {
+                cnt += (nums[j] == target ? 1 : -1);
+                if (cnt > 0) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String[] st = s.replaceAll("\\[","").replaceAll("]","").split(",");
+        int[] nums = new int[st.length];
+        for(int i = 0; i < st.length; i++){
+            nums[i] = Integer.parseInt(st[i]);
+        }
+        int t = in.nextInt();
+        System.out.println(countMajoritySubarrays(nums, t));
+    }
+}
